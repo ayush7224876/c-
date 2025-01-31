db434e93c8f5424bbc12b936d7b868cd0ca4de0b
sanidhya <ayushbpl52@gmail.com>
2025-01-31 11:21:02 +0530

    c-

---
+++ c++ jan 22/c.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int fibo(int n)
+{
+    if(n==1 || n==0) return n;
+    return fibo() 
+}
---
+++ c++ jan 23/c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void rev(string &str)
+{
+    int start=0;
+    int end=str.length()-1;
+    while(start<end)
+    {
+        char temp=str[start];
+        str[start]=st
+        r[end];
+        str[end]=temp;
+        start++;
+        end--;
+    }
+
+}
+int main(){
+    string str="jayes";
+    rev(str);
+    cout << str;
+}
